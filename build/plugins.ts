7d8a2eb7572d0d401fffdd29ae4b5e3e35808bf0
EvilMoOd <962475158@qq.com>
2023-11-04 21:07:49 +0800

    ⚡ 构建优化

--- build/plugins.ts
+++ build/plugins.ts
@@ -1,5 +1,44 @@
-export function getPluginsList(
-  command: string,
-  VITE_CDN: boolean,
-  VITE_COMPRESSION: ViteCompression,
-) {}
+import vue from "@vitejs/plugin-vue";
+import UnoCSS from "unocss/vite";
+import AutoImport from "unplugin-auto-import/vite";
+import Components from "unplugin-vue-components/vite";
+import { AntDesignVueResolver } from "unplugin-vue-components/resolvers";
+import svgLoader from "vite-svg-loader";
+import { viteMockServe } from "vite-plugin-mock";
+import vueJsx from "@vitejs/plugin-vue-jsx";
+
+export function pluginList(command: string) {
+  return [
+    vue({
+      script: {
+        defineModel: true,
+        propsDestructure: true,
+      },
+    }),
+    UnoCSS(),
+    // 自动导入模块
+    AutoImport({
+      imports: ["vue"],
+      resolvers: [AntDesignVueResolver()],
+      dts: "./types/auto-imports.d.ts",
+    }),
+    // 自动导入组件
+    Components({
+      resolvers: [
+        AntDesignVueResolver({
+          importStyle: false,
+        }),
+      ],
+      dts: "./types/auto-components.d.ts",
+    }),
+    // svg组件化支持
+    svgLoader(),
+    vueJsx(),
+    // mock支持
+    viteMockServe({
+      mockPath: "mock",
+      enable: command === "serve",
+      logger: false,
+    }),
+  ];
+}
