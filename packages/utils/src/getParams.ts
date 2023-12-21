9e9205c5c8d68836d85384a993659e0d42f8d415
EvilMoOd <962475158@qq.com>
2023-12-22 00:29:23 +0800

    ✨ getParams函数新增解析完整URL为Map

--- packages/utils/src/getParams.ts
+++ packages/utils/src/getParams.ts
@@ -3,8 +3,24 @@
  * @param url url
  * @returns 解析后参数
  */
-export const getParam = (url: string) => {
-  const r = new RegExp(`(\\?|#|&)${url}=([^&#]*)(&|#|$)`);
-  const m = location.href.match(r);
+export function getParamsMap(url: string) {
+  const reg = /([^?&=]+)=([^&]+)/g;
+  const paramsMap: Record<string, string | number> = {};
+  const match = [...url.matchAll(reg)];
+  match.forEach(([, k, v]) => {
+    Object.assign(paramsMap, { [k]: Number.isNaN(+v) ? v : +v });
+  });
+  return paramsMap;
+}
+
+/**
+ * 解析url中的某个参数
+ * @param key 某个参数
+ * @param url url
+ * @returns 解析后参数
+ */
+export const getParam = (key: string, url: string) => {
+  const r = new RegExp(`(\\?|#|&)${key}=([^&#]*)(&|#|$)`);
+  const m = url || location.href.match(r);
   return decodeURI(!m ? "" : m[2]);
 };
