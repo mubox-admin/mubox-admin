927d92fbae41bd3cb2491393cc2c1e67e207ef08
EvilMoOd <962475158@qq.com>
2024-04-20 17:50:13 +0800

    🔧 代码规则允许常量枚举

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,17 @@
 import antfu from "@antfu/eslint-config";
 
 export default antfu({
+  javascript: {
+    overrides: {
+      "no-restricted-syntax": [
+        "error",
+        "DebuggerStatement",
+        "LabeledStatement",
+        "WithStatement",
+        "TSExportAssignment",
+      ],
+    },
+  },
   stylistic: {
     quotes: "double",
     semi: true,
